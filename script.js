ff46bdcbf948f87da039988f2aa066441db8d631
GLBryan <garrettleebryan@gmail.com>
2021-09-10 23:02:22 -0400

    added global variables and functions ill need

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Gloval Variables
+var newPass;
+var lower;
+var upper;
+var numbers;
+var spec;
+var long;
+
+// Query for password type and the calls generatePassword()
+function getPassType() {
+
+  generatePassword()
+}
+
+// Generates password based on query
+function generatePassword(lower, upper, numbers, spec, long) {
+
+}
+
 // Write password to the #password input
 function writePassword() {
-    
-    var password = generatePassword()
+    getPassType()
+  
+    var password = newPass
     var passwordText = document.querySelector("#password");
   
     passwordText.value = password;
