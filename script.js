5f108066d4de20ee85af954c111bf7c24b43ef0f
GLBryan <garrettleebryan@gmail.com>
2021-09-10 23:31:56 -0400

    coded generatePassword()

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Gloval Variables
+// Global Variables
 var newPass;
 var lower;
 var upper;
@@ -9,7 +9,7 @@ var numbers;
 var spec;
 var long;
 
-// Query for password type and the calls generatePassword()
+// Query for password type, then calls generatePassword()
 function getPassType() {
 
   generatePassword()
@@ -18,6 +18,46 @@ function getPassType() {
 // Generates password based on query
 function generatePassword(lower, upper, numbers, spec, long) {
 
+  // variables
+  const specials = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  newPass = []
+  var Counter = long
+
+  // while loop for pass length
+  while (Counter > 0) {
+    
+    // if selected, characters log one at a time
+    if (lower == true) {
+      newPass += String.fromCharCode(Math.floor(Math.random() * 26) +97)
+      Counter -= 1
+      
+      // each log checked to ensure loop breaks
+      if (Counter == 0) {
+        break
+      }
+    }
+    if (upper == true) {
+      newPass += String.fromCharCode(Math.floor(Math.random() * 26) +65)
+      Counter -= 1
+      if (Counter == 0) {
+        break
+      }
+    }
+    if (numbers == true) {
+      newPass += (Math.floor(Math.random() * 10))
+      Counter -= 1
+      if (Counter == 0) {
+        break
+      }
+    }
+    if (spec == true) {
+      newPass += specials[(Math.floor(Math.random() * specials.length))]
+      Counter -= 1
+      if (Counter == 0) {
+        break
+      }
+    }
+  }
 }
 
 // Write password to the #password input
